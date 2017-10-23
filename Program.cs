eb48d058a469bd2714d0e71201a71fbbb835e221
metaboss <23161826+metaboss@users.noreply.github.com>
2017-10-22 22:17:08 -0800

    Fixed issue preventing multiple sources from processing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Collections.Generic;
 
 namespace hydroseed
 {
@@ -9,9 +10,12 @@ namespace hydroseed
         static void Main(string[] args)
         {
             SeedDB seedDB = new SeedDB();
+            List<SeedDB> dbs = new List<SeedDB>();
+
             if (args.Length > 0 && File.Exists(args[0]))
             {
                 seedDB = new SeedDB(File.ReadAllBytes(args[0]));
+                dbs.Add(seedDB);
             }
             else if (File.Exists("seedsources.txt"))
             {
@@ -25,6 +29,7 @@ namespace hydroseed
                     try
                     {
                         seedDB = new SeedDB(client.DownloadData(url));
+                        dbs.Add(seedDB);
                     }
                     catch (WebException we)
                     {
@@ -36,15 +41,18 @@ namespace hydroseed
             {
                 Console.WriteLine("No input file or seedsources.txt file found.");
             }
-            if (seedDB.Count > 0)
+            foreach (var db in dbs)
             {
-                var dir = Directory.CreateDirectory("fbi" + Path.DirectorySeparatorChar + "seed");
-
-                foreach (var seed in seedDB.Seeds)
+                if (db.Count > 0)
                 {
-                    File.WriteAllBytes(dir.FullName + Path.DirectorySeparatorChar + seed.TitleId + ".dat", seed.SeedValue);
+                    var dir = Directory.CreateDirectory("fbi" + Path.DirectorySeparatorChar + "seed");
+
+                    foreach (var seed in db.Seeds)
+                    {
+                        File.WriteAllBytes(dir.FullName + Path.DirectorySeparatorChar + seed.TitleId + ".dat", seed.SeedValue);
+                    }
+                    Console.WriteLine("Created dats for {0} titles.", db.Count);
                 }
-                Console.WriteLine("Created dats for {0} titles.", seedDB.Count);
             }
         }
     }
